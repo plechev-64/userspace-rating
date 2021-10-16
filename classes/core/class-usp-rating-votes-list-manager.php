7af8278be67c67e146d4bbc078b7b2e89d16dd72
Preci1 <35058782+Preci1@users.noreply.github.com>
2021-10-16 23:41:35 +0300

    update votes manager

--- classes/core/class-usp-rating-votes-list-manager.php
+++ classes/core/class-usp-rating-votes-list-manager.php
@@ -2,27 +2,32 @@
 
 class USP_Rating_Votes_List_Manager extends USP_Content_Manager {
 
-	public $object_author;
+	private $_required_params = [
+		'orderby'  => 'rating_date',
+		'order'    => 'DESC',
+		'pagenavi' => 1,
+		'search'   => 1
+	];
 
-	function __construct( $args = array() ) {
+	private $_custom_params = [
+		'object_type' => '_all'
+	];
 
-		$this->init_custom_prop( 'object_author', isset( $args['object_author'] ) ? $args['object_author'] : null );
+	public function __construct( array $args = [] ) {
 
-		parent::
-		__construct( array(
-			'number'  => 10,
-			'is_ajax' => 1,
-		) );
+		$this->set_custom_default_params( $this->_custom_params );
+
+		parent::__construct( array_merge( $this->_required_params, $args ) );
 
 	}
 
 	function get_query() {
 
-		$filter_by_type = $this->get_request_data_value( 'object_type' );
+		$filter_by_type = $this->get_param( 'object_type' );
 
-		if ( $filter_by_type ) {
+		if ( $filter_by_type !== '_all' ) {
 			$where = [
-				'object_author' => $this->object_author,
+				'object_author' => $this->get_param( 'object_author' ),
 				'object_type'   => $filter_by_type
 			];
 		} else {
@@ -37,36 +42,14 @@ class USP_Rating_Votes_List_Manager extends USP_Content_Manager {
 			}
 
 			$where = [
-				'object_author'   => $this->object_author,
+				'object_author'   => $this->get_param( 'object_author' ),
 				'object_type__in' => $object_type__in
 			];
 		}
 
 		return USP_Rating()->votes_query()
 		                   ->select( [] )
-		                   ->where( $where )
-		                   ->orderby(
-			                   $this->get_request_data_value( 'orderby', 'rating_date' ),
-			                   $this->get_request_data_value( 'order', 'DESC' )
-		                   );
-
-	}
-
-	function get_data_content() {
-
-		$content = '<div class="manager-content">';
-
-		if ( ! $this->data ) {
-			$content .= $this->get_no_result_notice();
-		} else {
-
-			$content .= usp_get_include_template( 'usp-rating-history-list.php', USP_RATING_BASE, [
-				'votes' => $this->data
-			] );
-		}
-		$content .= '</div>';
-
-		return $content;
+		                   ->where( $where );
 
 	}
 
@@ -74,41 +57,61 @@ class USP_Rating_Votes_List_Manager extends USP_Content_Manager {
 
 		$object_types = USP_Rating()->get_object_types()->get_all();
 
-		$object_types_list = [ '' => 'Все' ];
+		$object_types_list = [ '_all' => __( 'All', 'userspace-rating' ) ];
 
 		foreach ( $object_types as $object_type ) {
 			$object_types_list[ $object_type->get_id() ] = $object_type->get_name();
 		}
 
-		return array(
-			array(
+		return [
+			[
 				'type'   => 'select',
 				'slug'   => 'object_type',
-				'title'  => __( 'Тип объекта' ),
+				'title'  => __( 'Object type', 'userspace-rating' ),
 				'values' => $object_types_list,
-				'value'  => $this->get_request_data_value( 'object_type' )
-			),
-			array(
+				'value'  => $this->get_param( 'object_type' )
+			],
+			[
 				'type'   => 'select',
 				'slug'   => 'orderby',
-				'title'  => __( 'Сортировка по' ),
+				'title'  => __( 'Sort by', 'userspace-rating' ),
 				'values' => [
-					'rating_date'  => __( 'Дате оценки', 'wp-recall' ),
-					'rating_value' => __( 'Значению оценки', 'wp-recall' )
+					'rating_date'  => __( 'Vote date', 'userspace-rating' ),
+					'rating_value' => __( 'Vote value', 'userspace-rating' )
 				],
-				'value'  => $this->get_request_data_value( 'orderby', 'rating_date' ),
-			),
-			array(
+				'value'  => $this->get_param( 'orderby' ),
+			],
+			[
 				'type'   => 'radio',
 				'slug'   => 'order',
-				'title'  => __( 'Направление сортировки' ),
+				'title'  => __( 'Sorting direction', 'userspace-rating' ),
 				'values' => [
-					'DESC' => __( 'По убыванию' ),
-					'ASC'  => __( 'По возрастанию' )
+					'DESC' => __( 'Descending', 'userspace-rating' ),
+					'ASC'  => __( 'Ascending', 'userspace-rating' )
 				],
-				'value'  => $this->get_request_data_value( 'order', 'DESC' ),
-			)
-		);
+				'value'  => $this->get_param( 'order' ),
+			]
+		];
+
+	}
+
+	function get_items_content() {
+
+		$items = $this->get_items();
+
+		$content = '<div class="usp-content-manager-content">';
+
+		if ( ! $items ) {
+			$content .= $this->get_no_result_notice();
+		} else {
+
+			$content .= usp_get_include_template( 'usp-rating-history-list.php', USP_RATING_BASE, [
+				'votes' => $items
+			] );
+		}
+		$content .= '</div>';
+
+		return $content;
 
 	}
 
